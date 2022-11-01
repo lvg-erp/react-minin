9f8460b4ebd44f5af9808f88c493ca2dfaf894a6
lvg-erp <profmail@yandex.ru>
2022-11-01 13:04:18 +0300

    This is a new commit for what I originally planned to be amended

---
+++ src/hooks/products.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from "axios"
+import { useEffect, useState } from "react"
+import { IProduct } from "../models"
+
+export function useProducts() {
+    
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    function addProduct(product: IProduct) {
+      setProducts( prev => [...prev, product])
+    }
+  
+     async function fetchProducts() {
+  
+      try {
+        setError('')
+        setLoading(true)
+        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+        setProducts(response.data)
+        console.log(response)
+        setLoading(false)
+      } catch (e: unknown) {
+  
+        const error = e as AxiosError
+        setLoading(false)
+        setError(error.message)
+      }
+    }
+  
+    useEffect(()=> {
+      fetchProducts()
+      console.log('effect')
+    }, [])
+
+    return { products, error, loading, addProduct}
+}
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React  from "react"
+
+export function AboutPage() {
+
+    return (
+        <div>
+            lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime suscipit aliquam, obcaecati cum esse 
+        </div>
+    )
+
+}
